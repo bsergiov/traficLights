6dc8ac8461c254975694ac7336e17ab2819f9d2a
bsergiov <81264661+bsergiov@users.noreply.github.com>
2021-12-07 11:36:45 +0200

    add graphic and logic traffic light

--- traficLights/ViewController.swift
+++ traficLights/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redView: UIView!
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
+    @IBOutlet weak var changeButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = 0.1
+        orangeView.alpha = 0.1
+        greenView.alpha = 0.1
+        
+        redView.layer.cornerRadius = 70
+        orangeView.layer.cornerRadius = 70
+        greenView.layer.cornerRadius = 70
+        
     }
 
-
+    @IBAction func changedCollor() {
+        if changeButton.titleLabel?.text == "START"{
+            redView.alpha = 1
+            changeButton.setTitle("NEXT", for: .normal)
+             return
+        }
+        
+        if redView.alpha == 1 {
+            redView.alpha = 0.1
+            orangeView.alpha = 1
+            return
+        } else if orangeView.alpha == 1{
+            orangeView.alpha = 0.1
+            greenView.alpha = 1
+            return
+        } else if greenView.alpha == 1 {
+            greenView.alpha = 0.1
+            redView.alpha = 1
+            return
+        }
+    }
+    
 }
 
